2934c0ef1550e8067b17b0a3f5bd21addd5818d7
AnandDewangan <mr.anand.dewangan@gmail.com>
2023-02-18 22:32:10 +0530

    Red Bus Clone

--- src/components/App.js
+++ src/components/App.js
@@ -1,60 +1,38 @@
-import { useState, useEffect } from "react";
+import React from 'react'
 import '../styles/App.css';
+import Home from './Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SearchResult from './SearchResult';
+import { useState } from 'react';
+import JourneyContext from '../context/JourneyContext';
+import Journey from './Journey';
+import SeatSelection from './SeatSelection';
+import Login from './Login';
+
 const App = () => {
 
-  const [inputValue, setInputValue] = useState("");
-  const [wordCount, setWordCount] = useState(0);
-  const [charCount, setCharCount] = useState(0);
-  const [charLimit, setCharLimit] = useState(50);
-  const [fontSize, setFontSize] = useState(16);
+  const [fromCity, setFromCity] = useState('');
+  const [toCity, setToCity] = useState('');
 
-  function handleInput(event) {
-    setInputValue(event.target.value);
-  }
-
-  function handleCharLimit(event) {
-    setCharLimit(event.target.value);
-  }
-
-  function handleFontSize(event) {
-    setFontSize(event.target.value);
-  }
-
-  useEffect(() => {
-    const inputText = inputValue.trim();
-    const words = inputText.split(/\s+/).filter(word => word !== "").length;
-    const chars = inputText.length;
-
-    setWordCount(words);
-    setCharCount(chars);
-  }, [inputValue]);
-  
   return (
     <div id="main">
-      <textarea
-        value={inputValue}
-        onChange={handleInput}
-        style={{ fontSize: `${fontSize}px` }}
-      />
-
-      <div id="word-counter">Words: {wordCount}</div>
-      <div id="char-counter">Characters: {charCount}/{charLimit}</div>
-
-      <input
-        type="number"
-        value={charLimit}
-        onChange={handleCharLimit}
-        id="char-limit-input"
-      />
-
-      <input
-        type="range"
-        min="16"
-        max="32"
-        value={fontSize}
-        onChange={handleFontSize}
-        id="fontSize-input"
-      />
+      <JourneyContext.Provider value={
+        {
+          fromCity: fromCity,
+          toCity: toCity,
+          setFromCity: setFromCity,
+          setToCity: setToCity
+        }
+      }>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<Login/>}/>
+            <Route path='/home' element={<Home/>}/>
+            <Route path='/results' element={<Journey Component={SearchResult}/>} />
+            <Route path='/book-seats' element={<Journey Component={SeatSelection}/>} />
+          </Routes>
+        </BrowserRouter>
+      </JourneyContext.Provider>
     </div>
   )
 }
